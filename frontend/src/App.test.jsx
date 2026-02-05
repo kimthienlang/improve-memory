6d784901e2160baec904b6732310bb876fd95736
LE ANH TUAN 180303 <kimthienlang@gmail.com>
2026-02-05 23:11:17 +0700

    reset frontend

--- frontend/src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { add } from './add'
-
-describe('[ test add() :|| ]', () => {
-  it('add(1, -55) = 0', () => {
-    expect(add(1, -55)).toBe(0)
-  })
-})
--- frontend/src/add.js
+++
@@ -1,1 +0,0 @@
-export const add = (a, b) => a + b > 0 ? a + b : 0;
